60674fdd30bed719f5bde80f4a31064f50d01c28
jackie trillo <jackietrillo@hotmail.com>
2022-09-15 11:50:05 -0700

    moved object examples into seperate folder

--- sorting/binarySearch.js
+++ sorting/binarySearch.js
@@ -2,23 +2,35 @@ const binarySearch = (arr, key) => {
   let lo = 0;
   let hi = arr.length - 1;
 
-  while(lo <= hi) {
-    let mid = Math.floor((lo + hi) / 2)
-    if (key === arr[mid])
-       return mid;
+  while (lo <= hi) {
+    let mid = Math.floor((lo + hi) / 2);
+    if (key === arr[mid]) return mid;
     else if (key > arr[mid]) {
-        lo = mid + 1;
+      lo = mid + 1;
+    } else {
+      hi = mid - 1;
     }
-    else {
-        hi = mid - 1;
-    }      
   }
   return -1;
 };
 
+const binarySearchRec = (arr, lo, hi, key) => {
+  if (lo > hi) return -1;
+  let mid = Math.floor((lo + hi) / 2);
+  if (key === arr[mid]) {
+    return mid;
+  } else if (key > arr[mid]) {
+    lo = mid + 1;
+    return binarySearchRec(arr, lo, hi, key);
+  } else {
+    hi = mid - 1;
+    return binarySearchRec(arr, lo, hi, key);
+  }
+};
 
 const arr = [1, 2, 3, 4, 5, 6];
 
-const index = binarySearch(arr, 4);
+//const index = binarySearch(arr, 4);
+const index = binarySearchRec(arr, 0, arr.length - 1, 4);
 
 console.log(index);
